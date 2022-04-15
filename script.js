41b7f40d47d3d8022bc909b43f8e0ea9b045cdba
abhedanil <103728694+abhedanil@users.noreply.github.com>
2022-04-15 23:46:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function premium(){
+    alert("Please Wait")
+    window.location=("https://www.spotify.com/in-en/premium/")
+}
+function support(){
+    alert("Please Wait")
+    window.location=("https://support.spotify.com/in-en/?_gl=1*1sqe9dw*_gcl_aw*R0NMLjE2NTAwMzA0ODguQ2owS0NRandyLVNTQmhDOUFSSXNBTmh6dTE3Y09WcW1oeGg3LU81cE5QN3dCbDMyVGM4bnJBY0JZVTNZQnk0bDVnQ2NWZW9nWVFzbGZGa2FBaDVwRUFMd193Y0I.*_gcl_dc*R0NMLjE2NTAwMzA0ODguQ2owS0NRandyLVNTQmhDOUFSSXNBTmh6dTE3Y09WcW1oeGg3LU81cE5QN3dCbDMyVGM4bnJBY0JZVTNZQnk0bDVnQ2NWZW9nWVFzbGZGa2FBaDVwRUFMd193Y0I.")
+}
+function download(){
+    alert("Please Wait")
+    window.location=("https://www.spotify.com/in-en/download/windows/")
+}
+function signup(){
+    alert("Please Wait")
+    window.location=("https://www.spotify.com/in-en/signup")
+}
+function login(){
+    alert("Please Wait")
+    window.location=("https://accounts.spotify.com/en/login/?continue=https%3A//open.spotify.com/__noul__%3Fl2l%3D1%26nd%3D1&_locale=en-IN")
+}
